67e7cdd815c6a4fc1fc0e57840aa67e451af4eab
Phil Grayson <pgrayson@beluga3.int.ets1.calculquebec.ca>
2020-06-18 15:03:03 -0400

    working copy_stuff

--- copy_stuff.sh
+++ copy_stuff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J        # Name for the job (keep it short and informative)
-#SBATCH -N        # Number of nodes
-#SBATCH -n        # Use n cores
-#SBATCH -t        # Runtime in D-HH:MM 
-#SBATCH --mem     # Memory requested (mb default, or specify G for Gb) 
-#SBATCH -o        # File to which STDOUT will be written 
-#SBATCH -e        # File to which STDERR will be written 
-#SBATCH --account # Who are are going to charge it to?
+#SBATCH -J copyPtoS   # Name for the job (keep it short and informative)
+#SBATCH -N 1       # Number of nodes
+#SBATCH -n 1       # Use n cores
+#SBATCH -t 1-00:00     # Runtime in D-HH:MM 
+#SBATCH --mem=1G    # Memory requested (mb default, or specify G for Gb) 
+#SBATCH -o cp.%A.out       # File to which STDOUT will be written 
+#SBATCH -e cp.%A.err       # File to which STDERR will be written 
+#SBATCH --account=def-docker # Who are are going to charge it to?
 
-# do something
+for file in $(cat persons_samples.txt); do cp $file lamprey_June/; done
