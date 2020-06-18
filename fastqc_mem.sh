98f46bf727a622a5c2ecca7639bc19a15758eff2
Phil Grayson <pgrayson@fas.harvard.edu>
2020-06-18 14:19:49 -0500

    Update fastqc_mem.sh

--- fastqc_mem.sh
+++ fastqc_mem.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH -J fastqc
 #SBATCH -n 1                     # Use 1 cores for the job
-#SBATCH -t 0-02:59                 # Runtime in D-HH:MM
+#SBATCH -t 7-00:00                 # Runtime in D-HH:MM
 #SBATCH --mem=800               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o fastqc.%A.out  # File to which STDOUT will be written
 #SBATCH -e fastqc.%A.err  # File to which STDERR will be written
-#SBATCH --account=def-docker
+#SBATCH --account=#def-docker or def-coling
 
-# This was run on Graham
+# must be executed twice: 
+# sbatch fastqc_mem_long.sh R1
+# sbatch fastqc_mem_long.sh R2 
 
-module load fastqc
+module load fastqc/0.11.8
 
-fastqc -f fastq $1
-
+for file in $(ls *${1}*gz); do fastqc -f fastq $file; done
