de694c6b6f49ab6f52afc795a639a65a0d12b37a
Phil Grayson <pgrayson@fas.harvard.edu>
2020-06-24 17:06:41 -0500

    Update parsing_over_rep.R

--- parsing_over_rep.R
+++ parsing_over_rep.R
@@ -3,3 +3,5 @@ library(tidyverse)
 over_rep <- read_delim("~/Desktop/final_over_rep.txt",delim="\t", col_names = c("sequence","count","percentage","hit"))
 
 over_rep.unique <- over_rep %>% select(-count,-percentage) %>% unique()
+
+write_delim(over_rep.unique %>% select(sequence), "Over_sequence.txt")
