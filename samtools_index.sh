f2bde39d8ad6f9c368f6a363fbda813f851e799e
Phil Grayson <pgrayson@cedar1.cedar.computecanada.ca>
2020-07-21 13:08:22 -0700

    decreasing sam index time

--- samtools_index.sh
+++ samtools_index.sh
@@ -3,7 +3,7 @@
 #SBATCH -J sam_index 
 #SBATCH -N 1                      # Ensure that all cores are on one machine
 #SBATCH -n 1                # Use n cores for one job 
-#SBATCH -t 0-03:00                # Runtime in D-HH:MM 
+#SBATCH -t 0-00:30                # Runtime in D-HH:MM 
 #SBATCH --mem=4000            # Memory pool for all cores 
 #SBATCH -o sindex.%A.out       # File to which STDOUT will be written 
 #SBATCH -e sindex.%A.err       # File to which STDERR will be written 
